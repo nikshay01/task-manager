9db0a9d0f1ba3d5687662c62bcdc024d8cec0f5d
nikshay <vestige5586@gmail.com>
2025-09-03 22:28:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+
+
+const list = document.getElementById("task-list");
+let id_num = -1;
+let allTasks = {
+  name: "TaskNames",
+  progress: {},
+};
+let newTasks = {};
+function addTask() {
+  const task = prompt("add task");
+  if (task == "") {
+    alert("task can not be empty");
+  } else {
+    id_num += 1;
+    allTasks.progress[`task${id_num}`] = task;
+    newTasks.taskName = task;
+
+    list.innerHTML += `<input class='checkBox'id="check${id_num}" type="checkbox"><label id="task-${id_num}" class="task-name">${task}</label><br>`;
+    console.log(newTasks, allTasks);
+    sendData(5);
+  }
+}
+function sendData(typee) {
+  fetch("http://localhost:3000/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(typee == 5 ? newTasks : allTasks),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("message: ", data);
+    })
+    .catch((err) => console.log(err));
+}
+async function getData() {
+  let Data;
+  let res = await fetch("http://localhost:3000/receive/tasks/names");
+  Data = await res.json();
+  return Data;
+}
+getData().then((d) => {
+  let keyes = Object.keys(d);
+  keyes.forEach((element) => {
+    let el1=document.createElement('label')
+    el1.textContent= d[element]
+    let el2=document.createElement('br')
+    let el3=document.createElement('input')
+    el3.type = 'checkbox'
+    list.appendChild(el3)
+    list.appendChild(el1)
+    list.appendChild(el2)
+    console.log(d[element]);
+  });
+});
---
+++ backend/back.js
@@ -0,0 +1,97 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import cors from "cors";
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(cors());
+// DB things
+const uri = "mongodb://localhost:27017/";
+const client = new MongoClient(uri);
+let tasksCollection;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    const db = client.db("taskDB");
+    tasksCollection = db.collection("taskName");
+    console.log(" Connected to MongoDB");
+  } catch (err) {
+    console.error(" MongoDB connection error:", err);
+  }
+
+  async function nnn() {
+    await tasksCollection.deleteMany({});
+  }
+  // nnn();
+  frontendData();
+}
+async function frontendData() {
+try {
+  const frontData =  await tasksCollection.findOne({name:'TaskNames'});
+  console.log("frontData:", frontData);
+  app.get('/receive/tasks/names',(req,res)=>{
+    res.send(frontData.progress)
+    console.log("data sent to: http://localhost:3000/receive/tasks/names");
+  })
+} catch (error) {
+  console.log("frontend data sending error:  ",error);
+}
+
+}
+// app.get('/receive/tasks/names') 
+connectDB();
+app.get("/", (req, res) => {
+  res.send("hlo"); 
+  frontendData();
+});
+
+app.post("/", async (req, res) => {
+  console.log("recieved");
+  console.log("data: ", req.body);
+  let data = req.body;
+  try {
+    let dataSearch = await tasksCollection.findOne({ name: "TaskNames" });
+    console.log("datasearch matchcount", dataSearch);
+    if (dataSearch) {
+      UpdateData(dataSearch,data);
+    } else {
+      CreateData(data);
+    }
+  } catch (e) {
+    console.log("error in data collection: ", e);
+  }
+
+  res.json({ success: true, message: "Data received", received: req.body });
+});
+
+app.listen(PORT, () => console.log("server live at:", PORT));
+
+const UpdateData = async (data,d2) => {
+  console.log("dataUpdate function executed");
+
+  let MAX=0;
+  const keys = Object.keys(data.progress)
+  keys.forEach(k => {
+    let num = parseInt(k.substring(4));
+    if(num>MAX)MAX=num;
+  });
+  const nextKey = `task${MAX+1}`;
+
+  try {
+  await tasksCollection.updateOne({ name: "TaskNames" },{$set:{[`progress.${nextKey}`]:d2.taskName}});
+  } catch (error) {
+    console.log("cannot update: ", error);
+  }
+};
+
+
+const CreateData = async (data) => {
+  console.log("dataCreate function  executed");
+
+  try {
+    await tasksCollection.insertOne(data).then(console.log("data sent to db"));
+  } catch (error) {
+    console.log("cannot insert", error);
+  }
+};
