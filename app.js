f78291f16c43456d761acb374730059f5d466c29
nikshay <vestige5586@gmail.com>
2025-09-04 08:22:08 +0530

    frontend data handeling improved

--- app.js
+++ app.js
@@ -16,12 +16,16 @@ function addTask() {
     allTasks.progress[`task${id_num}`] = task;
     newTasks.taskName = task;
 
-    list.innerHTML += `<input class='checkBox'id="check${id_num}" type="checkbox"><label id="task-${id_num}" class="task-name">${task}</label><br>`;
+    // list.innerHTML += `<input class='checkBox'id="check${id_num}" type="checkbox"><label id="task-${id_num}" class="task-name">${task}</label><br>`;
     console.log(newTasks, allTasks);
     sendData(5);
   }
 }
 function sendData(typee) {
+  if(Object.keys(newTasks).length==0||newTasks.taskName == null){
+    alert("empty data cannot be sent");
+    return;
+  }else{
   fetch("http://localhost:3000/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -32,6 +36,8 @@ function sendData(typee) {
       console.log("message: ", data);
     })
     .catch((err) => console.log(err));
+    location.reload()
+  }
 }
 async function getData() {
   let Data;
